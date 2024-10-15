01aa706cb4d8239fb961a590488852084671887a
OlehVelychko <70134299+OlehVelychko@users.noreply.github.com>
2024-10-15 15:03:28 +0300

    adding first test bean and Spring classes

---
+++ src/main/java/ua/velychko/springcourse/TestSpring.java
@@ -0,0 +1,17 @@
+package ua.velychko.springcourse;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+
+        TestBean testBean = context.getBean("testBean", TestBean.class);
+
+        System.out.printf(testBean.getName());
+
+        context.close();
+    }
+}
