4872b8ac1dfada5d1fc777a8e53bf7aa3219bb01
OlehVelychko <70134299+OlehVelychko@users.noreply.github.com>
2024-10-16 14:40:27 +0300

    dependency injection

--- src/main/java/ua/velychko/springcourse/MusicPlayer.java
+++ src/main/java/ua/velychko/springcourse/MusicPlayer.java
@@ -1,14 +1,45 @@
 package ua.velychko.springcourse;
 
-public class MusicPlayer {
-    private final Music music;
+import java.util.ArrayList;
+import java.util.List;
 
-    // IoC
-    public MusicPlayer(Music music) {
-        this.music = music;
+public class MusicPlayer {
+    private List<Music> musicList = new ArrayList<>();
+    private String name;
+    private int volume;
+
+    public MusicPlayer() {
+    }
+
+    private static void accept(Music el) {
+        System.out.printf("Playing: %s\n", el.getSong());
+    }
+
+    public List<Music> getMusicList() {
+        return musicList;
+    }
+
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
     }
 
     public void playMusic() {
-        System.out.printf("Playing: %s", music.getSong());
+        musicList.forEach(MusicPlayer::accept);
     }
 }
